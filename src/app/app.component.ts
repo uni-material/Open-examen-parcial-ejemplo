c04a6772bc03491d8f0ab00104334030dccb43b8
Ro <u202213765@upc.edu.pe>
2024-10-02 13:34:19 -0500

    feat: toolbar ready with translation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,24 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatAnchor} from '@angular/material/button';
 import {NgForOf} from '@angular/common';
+import {TranslateService} from '@ngx-translate/core';
+import {
+  LanguageSwitcherComponentComponent
+} from './public/components/language-switcher.component/language-switcher.component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink, NgForOf],
+  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink, NgForOf, LanguageSwitcherComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hartfordopen';
+  title = 'hartford';
   options =[
     { path: '/home', title: 'Home'},
-    { path: '/coso', title: 'coso'}
+    { path: '/mental-state-exams', title: 'Mental State Exams'}
   ]
+
+
 }
