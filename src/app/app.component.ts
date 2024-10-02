b353b1679d48700f54486158993fa7a9ebf99165
Ro <u202213765@upc.edu.pe>
2024-10-02 11:31:57 -0500

    feat: toolbar ready.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatAnchor} from '@angular/material/button';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hartfordopen';
+  options =[
+    { path: '/home', title: 'Home'},
+    { path: '/coso', title: 'coso'}
+  ]
 }
